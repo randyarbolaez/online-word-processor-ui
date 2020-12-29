891b6f3559ac7b80df21037d2c78ca627e56fd95
Randy <randyarbolaez0@gmail.com>
2020-12-29 10:43:09 -0500

    hope it works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
+import ENV from "./env";
 
-function App() {
+const socket = io(ENV.backendAPIUrl);
+
+const App = () => {
+  const [numberOfUsers, setNumberOfUsers] = useState(0);
+  const [inputData, setInputData] = useState("");
+  const [username, setUsername] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    socket.on("user joined", (data) => {
+      setNumberOfUsers(data.numUsers);
+      setAllUsers(data.allUsers);
+    });
+
+    socket.on("user left", (data) => {
+      setNumberOfUsers(data.numUsers);
+      setAllUsers(data.usersLeft);
+    });
+
+    socket.on("login", (data) => {
+      setNumberOfUsers(data.numUsers);
+      setAllUsers(data.allUsers);
+    });
+
+    socket.on("new message", (data) => {
+      setInputData(data.message);
+      console.log("DATA", data); // data will be 'woot'
+    });
+  }, []);
+
+  const addUser = (e) => {
+    socket.emit("add user", username);
+    setIsUserLoggedIn(true);
+  };
+
+  const onUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onUserTyping = (e) => {
+    setInputData(e.target.value);
+    socket.emit("new message", e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isUserLoggedIn ? (
+          <div style={{ marginBottom: 100 }}>
+            {allUsers.map((username) => (
+              <span key={username}>| {username} </span>
+            ))}
+            <p>Number of users online - {numberOfUsers}</p>
+            <textarea
+              onChange={(e) => onUserTyping(e)}
+              value={inputData}
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+        ) : (
+          <div>
+            <input
+              placeholder="Login"
+              type="text"
+              value={username}
+              onChange={(e) => onUsername(e)}
+            />
+            <button onClick={addUser}>Login</button>
+          </div>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
